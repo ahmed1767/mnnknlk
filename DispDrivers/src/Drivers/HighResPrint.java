58a9d7457b213b0d731255304b0c22737c1e7f93
DESKTOP-VOQT66P\Ahmed Munna <munnaa329@gmail.com>
2018-04-16 23:44:51 +0600

    Print_classes(3rd_commit)

---
+++ DispDrivers/src/Drivers/HighResPrint.java
@@ -0,0 +1,12 @@
+package Drivers;
+
+public class HighResPrint implements Print {
+
+	@Override
+	public String PrintDriverName() {
+		// TODO Auto-generated method stub
+		return "This Is a High Res Print";
+	}
+	
+
+}
---
+++ DispDrivers/src/Drivers/LowResPrint.java
@@ -0,0 +1,11 @@
+package Drivers;
+
+public class LowResPrint implements Print{
+
+	@Override
+	public String PrintDriverName() {
+		// TODO Auto-generated method stub
+		return "This Is a Low Res Print";
+	}
+
+}
